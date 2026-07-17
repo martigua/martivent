2487e2c51bb833fecd9e7f9ca08a0149507c1404
PasNinii <nicrannou@gmail.com>
2026-07-17 19:58:02 +0200

    secrest

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from config.env import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m&+ghw#p4jf*=vk5yxqlicehmz_6lg_m#dm_$3(05x=d)=zl%1'
+SECRET_KEY = env.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.debug
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.hosts
 
 
 # Application definition
